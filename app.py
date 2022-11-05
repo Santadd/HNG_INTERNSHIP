6c233f430dc35a0a144ef71c24ed979d58d6854f
Santadd <hethere97@gmail.com>
2022-11-05 00:06:23 +0000

    Stage 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, json
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -14,9 +14,31 @@ payload = { "slackUsername": "duahdivine1",
            "bio": "I am a Backend Developer"
            }
 
-@app.route('/')
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 @cross_origin()
 def home():
+    if request.method == "POST":
+        data = request.get_data()
+        converted_data = json.loads(data.decode('utf-8'))
+        operation_type = converted_data['operation_type']
+        x = converted_data['x']
+        y = converted_data['y']
+        print(converted_data['operation_type'])
+        if operation_type.lower() == "multiplication":
+            print("Ujash")
+            res = x * y
+        elif operation_type.lower() == "addition":
+            res = x + y
+        elif operation_type.lower() == "subtraction":
+            res = x - y
+        
+        return {"slackUsername": "Santana", 
+                "result": res, 
+                "operation_type": converted_data['operation_type']
+                }
     return jsonify(payload)
 
 if __name__ == "__main__":
